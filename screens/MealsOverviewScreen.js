7eddf7aa826105bbb8db5d034779e026c2a81c24
Uriel Zacarias <urielzacarias@gmail.com>
2022-04-25 00:16:10 -0700

    created a category screen and a meals overview with navigation

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,46 @@
+import {View, FlatList, StyleSheet} from 'react-native'
+//import {useRoute}from "@react-navigation/native"
+import {MEALS} from '../data/dummy-data';
+import MealItem from '../components/MealItem'
+
+function MealsOverviewScreen({ route }) {
+  //  const route = useRoute();
+    const catId = route.params.categoryId;
+
+    const displayMeals = MEALS.filter((mealItem)=> {
+        return mealItem.categoryIds.indexOf( catId ) >= 0
+    });
+
+    function renderMealItem(itemData){
+        const item = itemData.item;
+
+        const mealItemProps = {
+          title: item.title,
+          imageUrl: item.imageUrl,
+          affordability: item.affordability,
+          complexity: item.complexity,
+          duration: item.duration
+        };
+
+        return <MealItem {...mealItemProps} />
+    }
+
+    return (
+        <View style={styles.container}>
+            <FlatList 
+                data={displayMeals} 
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItem}
+            />
+        </View>
+    )
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        padding: 16,
+    }
+})
