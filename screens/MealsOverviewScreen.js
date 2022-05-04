422684ca2b79d12527d729e97b25a19f2007ed0d
uriel zacarias <urielzacarias@gmail.com>
2022-05-04 08:23:00 -0700

    chaged background color default, and added dynamic titles for the navigator.

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,15 +1,26 @@
+import { useLayoutEffect } from 'react';
 import {View, FlatList, StyleSheet} from 'react-native'
 //import {useRoute}from "@react-navigation/native"
-import {MEALS} from '../data/dummy-data';
+import {CATEGORIES, MEALS} from '../data/dummy-data';
 import MealItem from '../components/MealItem'
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   //  const route = useRoute();
     const catId = route.params.categoryId;
 
     const displayMeals = MEALS.filter((mealItem)=> {
         return mealItem.categoryIds.indexOf( catId ) >= 0
     });
+
+    useLayoutEffect(() => {    
+        const categoryTitle = CATEGORIES.find((category ) => category.id === catId).title;
+
+        navigation.setOptions({
+            title: categoryTitle
+        })
+
+    },[catId, navigation])
+
 
     function renderMealItem(itemData){
         const item = itemData.item;
@@ -22,7 +33,7 @@ function MealsOverviewScreen({ route }) {
           duration: item.duration
         };
 
-        return <MealItem {...mealItemProps} />
+        return <MealItem {...mealItemProps} /> 
     }
 
     return (
